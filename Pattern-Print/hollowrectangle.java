a612eb289e4d177a7b9188e4cc6bb85053ccbdbf
Shubham Pandey <shubpandeyham@gmail.com>
2026-06-07 22:39:49 +0530

    Important Pattern Printing problem

---
+++ Pattern-Print/hollowrectangle.java
@@ -0,0 +1,17 @@
+public class hollowrectangle{
+    public static void pattern(int totRow, int totCols){
+        for(int i = 1 ; i <= totRow;i++){
+            for(int j = 1 ; j<= totCols;j++){
+                if(i==1||i==totRow || j == 1 || j == totCols){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+    public static void main(){
+        pattern(3,4);
+    }
+}
