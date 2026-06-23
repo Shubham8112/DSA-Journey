77476a6d2761598dc3ea629f1abe3ec0b7fc0022
Shubham Pandey <shubpandeyham@gmail.com>
2026-06-23 11:07:37 +0530

    advanced pattern problem

---
+++ Pattern-Print/Solid_Rhombus.java
@@ -0,0 +1,18 @@
+public class Solid_Rhombus{
+    public static void rhombus(int n){
+        for(int i = 1 ; i<=n ; i++){
+            //space
+            for(int j = 1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j = 1 ; j <= n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(){
+        rhombus(5);
+    }
+}
